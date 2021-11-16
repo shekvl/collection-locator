b654b4214475ffae69ce864fc3d887c92cea67cb
peisenke <p.eisenkeil@gmail.com>
2021-11-16 15:50:02 +0100

    Add xml export

--- app/src/main/java/com/anonymizerweb/anonymizerweb/controller/DefinitionController.java
+++ app/src/main/java/com/anonymizerweb/anonymizerweb/controller/DefinitionController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.bind.JAXBException;
 import java.io.IOException;
 import java.util.List;
 
@@ -46,7 +47,7 @@ public class DefinitionController {
     }
 
     @PostMapping("/new")
-    public String newPost(@ModelAttribute NewDefinitionCommand command, Model model) throws IOException {
+    public String newPost(@ModelAttribute NewDefinitionCommand command, Model model) throws IOException, JAXBException {
         Definition save = definitionService.save(command);
         model.addAttribute("command", command);
         return "redirect:/definitions/" + save.getId();
--- app/src/main/java/com/anonymizerweb/anonymizerweb/repositories/LoincRepository.java
+++ app/src/main/java/com/anonymizerweb/anonymizerweb/repositories/LoincRepository.java
@@ -18,7 +18,7 @@ public interface LoincRepository extends CrudRepository<Loinc, String> {
     @Override
     List<Loinc> findAll();
 
-    @Query("from Loinc l where UPPER(l.long_common_name) LIKE CONCAT('%',UPPER(:queryString),'%') ORDER BY l.loinc_num ASC")
+    @Query("from Loinc l where (UPPER(l.long_common_name) LIKE CONCAT('%',UPPER(:queryString),'%')) OR (UPPER(l.loinc_num) LIKE CONCAT('%',UPPER(:queryString),'%')) ORDER BY l.loinc_num ASC")
     List<Loinc> findForAutocomplete(@Param("queryString") String queryString, Pageable pageable);
 
 }
