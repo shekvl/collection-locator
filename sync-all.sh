e1b6786817f14a2da6bb8ec66dd305dcab4f357f
vladimir <volodymyr.shekhovtsov@aau.at>
2025-06-30 19:03:23 +0200

    script to sync subprojects via git

---
+++ sync-all.sh
@@ -0,0 +1,6 @@
+git subtree pull --prefix=project-collection-locator-impl collection-locator-impl master --squash
+git subtree pull --prefix=project-searching-in-the-omop-based-repository searching-in-the-omop-based-repository master --squash
+git subtree pull --prefix=project-index-definition index-definition main --squash
+git subtree pull --prefix=project-k-anonymity k-anonymity main --squash
+git push origin master
+
